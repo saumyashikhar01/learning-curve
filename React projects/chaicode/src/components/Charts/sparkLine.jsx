8310b01df85cd6ba07f6578e8d5cdfb36a592790
saumyashikhar01 <saumyashikharpaul2000@gmail.com>
2026-03-01 12:46:47 +0530

    chaicode initial commit

---
+++ React projects/chaicode/src/components/Charts/sparkLine.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const sparkLine = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default sparkLine
---
+++ React projects/chaicode/src/components/ChartsHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChartsHeader = () => (
+  <div className=' mb-10'>
+    <div>
+      <p className='text-lg text-gray-400'>Chart</p>
+      </div>
+      <p className='text-center dark:text-gray-200 text-xl mb-2 mt-3'>{title||"anydata"}</p>
+    </div>
+);
+export default ChartsHeader;
